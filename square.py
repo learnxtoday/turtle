9e4a95a0cb97e65e04ab528f5d0e2f9c419bb0db
Parth Krishna <parthkrishna1611@gmail.com>
2019-01-06 10:50:44 +0530

    added functions, and colors

--- square.py
+++ square.py
@@ -1,13 +1,32 @@
 import turtle
 
+# function to make a square. Initialize a turtle instance, and pass it in the function. 
+def square(bob):
+    bob.begin_fill()
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.end_fill()
+
+# moves cursor down to create a new shape
+def moveDown(bob):
+    bob.penup()
+    bob.forward(150)
+    bob.pendown()
+
+
 bob = turtle.Turtle()
-bob.forward(100)
-bob.left(90)
-bob.forward(100)
-bob.left(90)
-bob.forward(100)
-bob.left(90)
-bob.forward(100)
+bob.color("black", "red")
+square(bob)
+
+moveDown(bob)
+
+bob.color("black", "green")
+square(bob)
 
 
 turtle.done()
