5af0bc688eeb7690333c4b0d942ce5889e5f31da
Parth Krishna <thenerdsuperuser@gmail.com>
2020-11-15 20:52:01 +0530

    threw in some colors.

---
+++ color.py
@@ -0,0 +1,21 @@
+import turtle
+from time import sleep
+
+
+tu = turtle.Turtle()
+tu.color('green')
+tu.pensize(8)
+tu.shape('turtle')
+
+tu.forward(100)
+tu.left(90)
+tu.penup()
+tu.forward(100)
+tu.pendown()
+tu.left(90)
+tu.color('orange')
+tu.forward(100)
+tu.left(90)
+
+
+sleep(4)
